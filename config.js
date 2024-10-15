384d18520c0588b8d5a63d54d9ea25f76ac4ad9a
KRISHNA <121825759+krishnaser@users.noreply.github.com>
2024-10-15 19:44:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/eypz/1727903040279_XEWG3FC67.png",
-ALIVE_MSG: process.env.ALIVE_MSG || " *CRONAZ-XD ALIVE🌸*\n\n*Owner:* Cronex\n\n_Nothing Is Impossible. Motivate Your Own Self🙈_ ",
-OWNER_NAME: process.env.OWNER_NAME || "𝑾𝒉𝒊𝒕𝒆 𝒔𝒆𝒓",
+ALIVE_MSG: process.env.ALIVE_MSG || " *OREKI-XD ALIVE🌸*\n\n*Owner:* Wizzkid\n\n_Nothing Is Impossible. Motivate Your Own Self🙈_ ",
+OWNER_NAME: process.env.OWNER_NAME || "𝐖𝐢𝐳𝐳𝐤𝐢𝐝",
 MENU: process.env.MENU || "https://ironman.koyeb.app/ironman/anime/waifu?type=long_hair",
-BOT_NAME: process.env.BOT_NAME || "CRONAZ-XD",
+BOT_NAME: process.env.BOT_NAME || "OREKI-XD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 wait: "⏳",
 done: "✅",
